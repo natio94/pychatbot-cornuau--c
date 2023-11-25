5b5718b6b53a06541b18156530a8d8a0510f6c70
natio94 <natemps.io@gmail.com>
2023-11-25 12:45:54 +0100

    Modification et correction des fonctions scoreTF scoreIDF et matTF_IDF

--- fonctionsTF_IDF.py
+++ fonctionsTF_IDF.py
@@ -1,47 +1,53 @@
 from math import log
 from fonctionsBase import *
-def frequence(directory:str):
-    """Retourne un dictionnaire contenant les fréquences TF des mots du document"""
+
+def scoreTF(document:str):
+    """Retourne un dictionnaire contenant les scores TF des mots du document"""
     freq = {}
-    documents = listOfFiles(directory, "txt")
-    for document in documents:
-        with open(directory+"\\"+document,"r",encoding="utf-8") as speech:
-            doc=speech.read()
-            for mot in doc.split(" "):
-                if mot not in freq:
-                    freq[mot] = 1
-                else:
-                    freq[mot] += 11
+    with open(".\cleaned" + "\\" + document, "r", encoding="utf-8") as speech:
+        doc = speech.read()
+        for mot in doc.split(" "):
+            if mot not in freq:
+                freq[mot] = 1
+            else:
+                freq[mot] += 1
     return freq
 
-def frequence_inverse(directory:str):
-    """Retourne un dictionnaire contenant les fréquences inverses IDF des mots du document"""
-    freq=frequence(directory)
-    freqInv={}
-    for mot in freq.keys():
-        freqInv[mot]=log(8/freq[mot])
+def scoreIDF(directory:str):
+    """Retourne un dictionnaire contenant les scores IDF des mots du dossier directory"""
+    freqInv = {}
+    documents = listOfFiles(directory, "txt")
+    for document in documents:
+        with open(directory + "\\" + document, "r",encoding="utf-8") as speech:
+            doc = speech.read()
+            for mot in doc.split(" "):
+                if mot not in freqInv:
+                    freqInv[mot] = 1
+                else:
+                    freqInv[mot] += 1
+    for mot in freqInv.keys():
+        freqInv[mot]=log(len(documents)/freqInv[mot]+1)
     return freqInv
 
-def tf_idf(directory:str):
-    """Retourne un dictionnaire contenant les fréquences TF-IDF des mots du document soit le produit des fréquences TF et IDF"""
-    freq=frequence(directory)
-    freqInv=frequence_inverse(directory)
-    tfidf={}
-    for mot in freq.keys():
-        tfidf[mot]=freq[mot]*freqInv[mot]
-    return tfidf
 
 def matTF_IDF(directory:str):
-    """Retourne une matrice contenant les fréquences TF-IDF des mots de chaque document du dossier et une list avec les mots dans le meme ordre que la matricela fonction"""
+    """Retourne une matrice contenant les fréquences TF-IDF des mots de chaque document du dossier et une list avec les mots dans le meme ordre que la matrice"""
     documents = listOfFiles(directory, "txt")
     matrice = []
     for document in documents:
         with open(directory + "\\" + document, "r",encoding="utf-8") as speech:
             doc = speech.read()
-            tf_idfMot= tf_idf(directory)
-            ligne = [mot for mot in tf_idfMot.keys()]
-            listMot=list(tf_idfMot.keys())
+            colonne = []
+
+            freqInv=scoreIDF(directory)
+            freq=scoreTF(document)
+            for mot in freqInv.keys():
+                if mot in freq.keys():
+                    score=freq[mot]*freqInv[mot]
+                else:
+                    score=0
+                colonne.append(score)
+            matrice.append(colonne)
+            listMot=list(freqInv.keys())
     return matrice,listMot
-
-
 
