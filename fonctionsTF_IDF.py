6f8f41b3da62fdce186c7cde588ce058917ecfd7
Jb7515 <jean-baptiste.duraiswamy@efrei.net>
2023-11-26 22:50:35 +0100

    Modification du main et ajout de nouvelles fonctions et rectifications

--- fonctionsTF_IDF.py
+++ fonctionsTF_IDF.py
@@ -5,8 +5,8 @@ def scoreTF(document:str):
     """Retourne un dictionnaire contenant les scores TF des mots du document"""
     freq = {}
     with open(".\cleaned" + "\\" + document, "r", encoding="utf-8") as speech:
-        doc = speech.read()
-        for mot in doc.split(" "):
+        docu = speech.read()
+        for mot in docu.split(" "):
             if mot not in freq:
                 freq[mot] = 1
             else:
@@ -35,19 +35,17 @@ def matTF_IDF(directory:str):
     documents = listOfFiles(directory, "txt")
     matrice = []
     for document in documents:
-
-        colonne = []
-
-        freqInv=scoreIDF(directory)
-        freq=scoreTF(document)
-        for mot in freqInv.keys():
-            if mot in freq.keys():
-                score=freq[mot]*freqInv[mot]
-            else:
-                score=0
-            colonne.append(score)
-        matrice.append(colonne)
-        listMot=list(freqInv.keys())
+            colonne = []
+            freqInv=scoreIDF(directory)
+            freq=scoreTF(document)
+            for mot in freqInv.keys():
+                if mot in freq.keys():
+                    score=freq[mot]*freqInv[mot]
+                else:
+                    score=0
+                colonne.append(score)
+            matrice.append(colonne)
+            listMot=list(freqInv.keys())
     return matrice,listMot
 
 
@@ -63,7 +61,8 @@ def motsMoinsImportants(directory):
     return mots_non_importants
 
 
-def motPlusElevéTFIDF(directory):
+def motPlusEleveTFIDF(directory:str):
+    """Retourne un liste contenant le ou les mots ayant le score TF-IDF le plus élévé"""
     matrice, listMot = matTF_IDF(directory)
     mots_max_tfidf = []
 
@@ -76,40 +75,77 @@ def motPlusElevéTFIDF(directory):
     return mots_max_tfidf
 
 
-def presidentsParlantDeNation(directory, mot="nation"):
+def presidentsParlantDeMot(directory:str, mot:str):
+    """Retourne la liste des présidents parlant d'un mot donné et le président l'ayant dit le plus de fois'"""
     documents = listOfFiles(directory, "txt")
-    presidents_occurrences = {}
+
+    presidents=listPresident()
+    presidentsOccurrences = {president:0 for president in presidents}
+    for i in range(len(documents)):
+        document=documents[i]
+        if i<=1:
+            president = presidents[0]
+        elif i<=4:
+            president = presidents[i-1]
+        elif i ==5:
+            president=presidents[4]
+        elif i>5:
+            president=presidents[5]
+        with open(directory + "\\" + document, "r", encoding="utf-8") as speech:
+            docu=speech.read()
+            if mot in document:
+                presidentsOccurrences[president] += docu.count(mot)
+        presToRemove=[]
+
+    for pres in presidentsOccurrences:
+        if presidentsOccurrences[pres] == 0:
+            presToRemove.append(pres)
+    for pres in presToRemove:
+        presidentsOccurrences.pop(pres)
+    presidentsOccurrences1 = sorted(presidentsOccurrences.items(), key=lambda x: x[1], reverse=True)
+
+    return list(presidentsOccurrences.keys()),presidentsOccurrences1[0][0]
+
+
+
+def premierPresidentParlantDeMots(directory:str, mots:list):
+    """Retourne le premier président parlant d'une list de mot donnée"""
+    documents = listOfFiles(directory, "txt")
 
     for document in documents:
         with open(directory + "\\" + document, "r", encoding="utf-8") as speech:
-            doc = speech.read().lower()
-            if mot in doc:
-                president = document.split("_")[1][:-4]
-                if president not in presidents_occurrences:
-                    presidents_occurrences[president] = doc.count(mot)
+            docu = speech.read()
+            if any(mot in docu for mot in mots):
+                presDit.append(president)
+    for pres in ordrePresChrono:
+        if pres in presDit:
+            return pres
 
-    presidents_occurrences = sorted(presidents_occurrences.items(), key=lambda x: x[1], reverse=True)
-    return presidents_occurrences
+def motsEvoquesParTous():
+    """Retourne la liste des mots évoqués par tous les présidents"""
+    matrice,listeMot=matTF_IDF("./cleaned")
+    mots=[]
+    for i in range(len(listeMot)):
+        if (matrice[0][i]!=0 or matrice[1][i]!=0) and matrice[2][i]!=0 and matrice[3][i]!=0 and matrice[4][i]!=0 and (matrice[5][i]!=0 or matrice[6][i]!=0):
+            mots.append(listeMot[i])
+    return mots
 
+def motsPlusRepetesChirac():
+    freq1 = scoreTF("Nomination_Chirac1.txt")
+    freq2 = scoreTF("Nomination_Chirac2.txt")
+    freq3 = {}
 
-def premierPresidentParlantDeClimatEcologie(directory, mots=["climat", "écologie"]):
-    documents = listOfFiles(directory, "txt")
+    for mot in freq1.keys():
+        if mot in freq2.keys():
+            freq3[mot] = freq1[mot] + freq2[mot]
+    motsPlusRepetes=[]
+    maxScore=0
+    for mot in freq3.keys():
+        score = freq3[mot]
+        if maxScore < score:
+            maxScore = score
+            motsPlusRepetes = [mot]
+        elif maxScore == score:
+            motsPlusRepetes.append(mot)
 
-    for document in documents:
-        with open(directory + "\\" + document, "r", encoding="utf-8") as speech:
-            doc = speech.read().lower()
-            if any(mot in doc for mot in mots):
-                president = document.split("_")[1][:-4]
-                return president
-
-def motsEvoquesParTous(directory, mots_non_importants):
-    documents = listOfFiles(directory, "txt")
-    mots_evoques_tous = set()
-
-    for document in documents:
-        with open(directory + "\\" + document, "r", encoding="utf-8") as speech:
-            doc = speech.read().lower().split()
-
-            mots_evoques_tous.update(set(doc) - set(mots_non_importants))
-
-    return list(mots_evoques_tous)
+    return motsPlusRepetes
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from fonctionsBase import *
 from fonctionsTF_IDF import *
-#print(listOfFiles(".\speeches","txt"))
-#print(listPresident())
-reloadTexte()
-fichiersToMin() #ne marche pas car lower cause des problèmes avec les accents (encodage)
- #pour retester les fonctions de modification des fichiers
-cleanFile()
 
-#with open(".\cleaned\\Nomination_Macron.txt","r") as speech:
-#speechTexte=speech.read()
-print(frequence(".\cleaned"))
 
-#print(frequence_inverse(".\cleaned"))
+print(motsMoinsImportants(".\cleaned"))
 
-#print(max(tf_idf(".\cleaned").items()), lambda x: x[1])
+print(motPlusEleveTFIDF(".\cleaned"))
 
-#print(matTF_IDF(".\cleaned")[0:2])
+print(motsPlusRepetesChirac())
+
+print(presidentsParlantDeMot(".\cleaned","nation"))
+print(listPresident())
+
+print(premierPresidentParlantDeMots(".\cleaned",["écologie","climat"]))
+
+print(motsEvoquesParTous())
+
+
