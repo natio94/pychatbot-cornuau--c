d851ebb6fbe85a910fca32cd6b97ba51b3813dd6
natio94 <natemps.io@gmail.com>
2023-12-07 15:20:07 +0100

    correction du main

--- fonctionsBase.py
+++ fonctionsBase.py
@@ -1,13 +1,16 @@
 import os
 
-def listOfFiles(directory, extension):
+def listOfFiles(directory:str, extension:str):
+    """Retourne une liste contenant les noms des fichiers du dossier "directory" ayant l'extension "extension"""
     filesNames=[]
     for file in os.listdir(directory):
         if file.endswith(extension):
             filesNames.append(file)
     return filesNames
 
+
 def listPresident():
+    """Retourne une liste des noms des présidents et les mets dans le dossier "speeches" """
     filesNames=listOfFiles(".\speeches","txt")
     presidents= {}
     for file in filesNames:
@@ -19,7 +22,27 @@ def listPresident():
             presidents[president]=""
     return presidents
 
+def associerNomPrenom():
+    """Associe les noms des president à leurs prenoms """
+    nomPrenomPresident = {}
+    for nomPresident in listPresident():
+        if nomPresident == "Macron":
+            nomPrenomPresident[nomPresident]="Emmanuel"
+        elif nomPresident == "Hollande":
+            nomPrenomPresident[nomPresident]="François"
+        elif nomPresident ==  "Sarkozy":
+            nomPrenomPresident[nomPresident]="Nicolas"
+        elif nomPresident == "Chirac":
+            nomPrenomPresident[nomPresident]="Jacques"
+        elif nomPresident == "Mitterand":
+            nomPrenomPresident[nomPresident]="François"
+        elif nomPresident == "Giscard dEstaing":
+            nomPrenomPresident[nomPresident]="Valerie"
+    return nomPrenomPresident
+
+
 def fichiersToMin():
+    """Permet de convertir les 8 fichiers dans le dossiers speeches en minuscule et les enregistre dans le dossier cleaned"""
     filesNames=listOfFiles(".\speeches","txt")
     for file in filesNames:
         with open(".\speeches\\"+file,"r",encoding="utf-8") as speech:
@@ -30,6 +53,7 @@ def fichiersToMin():
             speech.write(speechTexte)
 
 def cleanFile():
+    """Remplace ou suprime les caractères spéciaux et les sauts de lignes"""
     filesNames=listOfFiles(".\cleaned","txt")
     for file in filesNames:
         with open(".\cleaned\\"+file,"r",encoding="utf-8") as speech:
@@ -41,14 +65,3 @@ def cleanFile():
             speechTexte=speechTexte.replace('\n'," ")
         with open(".\cleaned\\"+file,"w",encoding="utf-8") as speech:
             speech.write(speechTexte)
-
-def reloadTexte():
-    filesNames = listOfFiles(".\speeches", "txt")
-    for file in filesNames:
-        with open(".\speeches\\" + file, "r",encoding="utf-8") as speech:
-            speechTexte = speech.read()
-
-        with open(".\cleaned\\" + file, "w",encoding="utf-8") as speech:
-            speech.write(speechTexte)
-
-
--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-from fonctionsBase import *
-#print(listOfFiles(".\speeches","txt"))
-#print(listPresident())
 
-fichiersToMin()
-cleanFile()
+rep=input("Quel fonction voulez vous utiliser ?\n1:fonctions de base\n2:fonctions TF_IDF\n")
+#fonctions de base
+if rep=="1":
+    from fonctionsBase import *
+    rep=input("Que voulez vous faire ?\n1:Liste des présidents\n2:Associer nom et prénom\n3:Convertir les fichiers en minuscule\n4:Nettoyer les fichiers\n")
+    if rep=="1":
+        print(listPresident())
+    elif rep=="2":
+        print(associerNomPrenom())
+    elif rep=="3":
+        fichiersToMin()
+    elif rep=="4":
+        cleanFile()
 
-#with open(".\cleaned\\Nomination_Macron.txt","r") as speech:
-#speechTexte=speech.read()
+#fonctions TF_IDF
+elif rep=="2":
+    from fonctionsTF_IDF import *
+    rep=input("Que voulez vous faire ?\n1:Mots les moins importants\n2:Mot avec le score TF-IDF le plus élevé\n3:Présidents parlant d'un mot\n4:Premier président parlant de mots\n5:Mots évoqués par tous\n")
+    if rep=="1":
+        print(motsMoinsImportants("./cleaned"))
+    elif rep=="2":
+        print(motPlusEleveTFIDF("./cleaned"))
+    elif rep=="3":
+        mot=input("Quel mot voulez vous chercher ?\n")
+        print(presidentsParlantDeMot("./cleaned",mot))
+    elif rep=="4":
+        mots=[]
+        nbMots=int(input("Combien de mots voulez vous chercher ?\n"))
+        for i in range(nbMots):
+            mots.append(input("Quel mot voulez vous chercher ?\n"))
+        print(premierPresidentParlantDeMots("./cleaned",mots))
+    elif rep=="5":
+        print(motsEvoquesParTous())
